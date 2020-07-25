d41595f229e67ec5df25e8ec0e47d9e72fca04a1
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-25 17:00:30 +0000

    add psql tables and testing records

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,10 +1,10 @@
 -- Drop and recreate Users table (Example)
 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
-);
+-- DROP TABLE IF EXISTS users CASCADE;
+-- CREATE TABLE users (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   name VARCHAR(255) NOT NULL
+-- );
 
 /*
 NY notes:
@@ -15,3 +15,45 @@ I will create 4 TABLES here:
 - ordered_items
 */
 
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS menu_items CASCADE;
+DROP TABLE IF EXISTS ordered_items CASCADE;
+
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  role TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+order_time VARCHAR(255) NOT NULL, -- NOW()
+wait_time INTEGER, -- should we delete it?
+order_status TEXT NOT NULL,
+pickup_time  VARCHAR(255) NOT NULL, -- NOW()
+customer_notes TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS menu_items CASCADE;
+CREATE TABLE menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR (255) NOT NULL,
+  thumbnail_url VARCHAR (255) NOT NULL,
+  price INTEGER NOT NULL,
+  description TEXT,
+  category VARCHAR (255) NOT NULL,
+
+DROP TABLE IF EXISTS ordered_items CASCADE;
+CREATE TABLE ordered_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id NTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  menu_item_id INTEGER REFERENCES menu_items(id),
+  qty INTEGER NOT NULL
+);
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,7 @@
 -- Users table seeds here (Example)
 
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- INSERT INTO users (name) VALUES ('Alice');
+-- INSERT INTO users (name) VALUES ('Kira');
 
 /*
 NY notes:
@@ -11,3 +11,16 @@ I will populate 4 TABLES here by inserting records for:
 - orders
 - ordered_items
 */
+
+INSERT INTO users (name, email, password, phone, role)
+  VALUES ('Natalia', 'natalia.yuzhakova@gmail.com', 'password', '+16048805245', 'customer');
+
+INSERT INTO orders (uder_id, order_time, wait_time, order_status, pickup_time, customer_notes)
+  VALUES ('10', '07/25/2020, 11:30 AM', 15, 'complete', '07/25/2020, 11:45 AM', 'extra dressing');
+
+INSERT INTO menu_items (name, thumbnail_url, price, description, category)
+  VALUES ('Roots Salad', url, 8.99, 'gardem salad mix with seasonal vegetables and balsamic venegraitte', 'appetizer');
+
+INSERT INTO ordered_items (order_id, menu_item_id, qty)
+  VALUES (10, 1, 2);
+
