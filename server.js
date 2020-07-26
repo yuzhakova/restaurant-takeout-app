21b64953a9dd5fc7e705758a56423cb83e2f6180
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-26 19:52:41 +0000

    test branch X4

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use("/confirmation", confirmationRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
-// aHome page
+// Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
