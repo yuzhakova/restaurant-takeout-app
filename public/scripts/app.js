3b5e8c19486ea334d620d6fd34ec68505e7d9af2
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-27 00:12:17 +0000

    add jquery to menu page, to load from db

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,37 @@
-/*$(() => {
+$(() => {
+
+  const createMenuItemElement = function(item) {
+
+    const stringifiedItems =
+      `<img  class="user" src="${item.thumbnail_url}">
+       <p class="item-name">${item.name}</p>
+       <p class="item-description">${item.description}</p>
+       <p class="item-price">>${item.price}</p>
+        `;
+
+    return $(stringifiedItems);
+  };
+
+
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});*/
+    url: "/menu/1"
+  }).done(res => {
+      let menuItems = res.templateVars;
+      for (let item = 0; item < menuItems.length; item++) {
+        if (menuItems[item].category === 'main') {
+          const $menuItem = createMenuItemElement(menuItems[item]);
+          $('#menu_item_mains').append($menuItem);
+        } else if (menuItems[item].category === 'soup') {
+          const $menuItem = createMenuItemElement(menuItems[item]);
+          $('#menu_item_soups').append($menuItem);
+        } else if (menuItems[item].category === 'dessert') {
+          const $menuItem = createMenuItemElement(menuItems[item]);
+          $('#menu_item_dessert').append($menuItem);
+        }
+      }
+    });;
+});
+
+
+
