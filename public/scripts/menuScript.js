399c00f9332586e69de61bd6f5c874be433559e6
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-28 04:18:51 +0000

    commit

---
+++ public/scripts/menuScript.js
@@ -0,0 +1,67 @@
+$(() => {
+
+  const createMenuItemElement = function(item) {
+
+    const stringifiedItems =
+      `<div class="menu_display">
+          <img  class="user" src="${item.thumbnail_url}">
+          <div class="menu-info-and-button">
+            <div class="menu_info">
+              <p class="item-name">${item.name}</p>
+              <p class="item-description">${item.description}</p>
+              <p class="item-price">$ ${(item.price / 100)}</p>
+            </div>
+            <button data-product-id="${item.id}" class="add-to-cart" type="button" class="btn btn-success">Add To Cart</button>
+          </div>
+       </div>
+        `;
+
+    return $(stringifiedItems);
+  };
+
+
+  $.ajax({
+    method: "GET",
+    url: "/menu/1"
+  })
+    .done(res => {
+      let menuItems = res.templateVars;
+      for (let item = 0; item < menuItems.length; item++) {
+        if (menuItems[item].category === 'main') {
+          const $menuItem = createMenuItemElement(menuItems[item]);
+          $('#menu_item_mains').append($menuItem);
+        } else if (menuItems[item].category === 'soup') {
+          const $menuItem = createMenuItemElement(menuItems[item]);
+          $('#menu_item_soups').append($menuItem);
+        } else if (menuItems[item].category === 'dessert') {
+          const $menuItem = createMenuItemElement(menuItems[item]);
+          $('#menu_item_desserts').append($menuItem);
+        }
+      }
+
+
+      $(".add-to-cart").click(function(event) {
+        const menuId = $(this).data("product-id")
+        const itemName = menuItems[menuId - 1].name;
+        const itemPrice = ((menuItems[menuId - 1].price) / 100);
+        event.preventDefault();
+
+          $.ajax({
+            method: 'POST',
+            url: '/checkout',
+            data: { name: itemName, price: itemPrice }
+          })
+          .done(res => {
+            console.log(res.selectedMenuItem)
+          })
+
+        });
+    });
+
+
+
+
+});
+
+
+
