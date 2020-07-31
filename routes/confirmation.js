43142175903071823716fef30c05d744260fbdd7
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-31 18:01:46 +0000

    add Twilio capabilities

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -6,7 +6,15 @@ module.exports = (db) => {
   let confirmedItems = {};
   let prices = {};
   let customerInfo = {};
+  let totalQuantity = 0;
+  let waitTime = 0;
 
+
+
+  const accountSid = 'ACdf5a7fd0dd98ae7a672b705a9bcb9591';
+  const authToken = '8aae7aa726fe12ad008b759dabd13731';
+  const twilio = require('twilio');
+  const client = new twilio(accountSid, authToken);
 
 
   router.post("/", (req, res) => {      // JSON-only route for AJAX POST
@@ -15,8 +23,9 @@ module.exports = (db) => {
     for (let item in items) {
       confirmedItems[item] = {};
       confirmedItems[item].id = items[item].id;
-      confirmedItems[item].qty = items[item].qty;
+      confirmedItems[item].qty = Number(items[item].qty);
       confirmedItems[item].image = items[item].image;
+      totalQuantity += confirmedItems[item].qty;
     }
     let subPrice = ((selectedMenuItem.subTotalPrice) / 100).toFixed(2);
     let totalPrice = (subPrice * 1.12).toFixed(2);
@@ -29,6 +38,20 @@ module.exports = (db) => {
   });
 
   router.get("/", (req, res) => {
+    if (totalQuantity > 3) {
+      waitTime = totalQuantity * 6;
+    } else {
+      waitTime = 15;
+    }
+    client.messages
+    .create({
+      body: `Your FEED-ly order will be ready in ${waitTime} minutes.`,
+      from: '16042659121',
+      to: `16043680803`
+    }).then((message) => {
+      console.log(message.sid)
+    })
+
     res.render("confirmation", { confirmedItems, prices, customerInfo });
     });
   return router;
