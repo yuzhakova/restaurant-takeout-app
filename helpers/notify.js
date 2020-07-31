91528417f2e9a78da9f516cdd04758870952c775
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-31 06:33:38 +0000

    need to set up twilio

--- helpers/notify.js
+++ helpers/notify.js
@@ -2,54 +2,58 @@
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure //
 
-const twilioSMSAPI = () => {
-  const accountSid = process.env.TWILIO_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+// const twilioSMSAPI = () => {
+  const accountSid = 'ACdaab8cf6fda53affb3d835ce9bf89163';
+  const authToken = '7bc370df179ce5ec00ad781031f2dabf';
+  // const accountSid = process.env.TWILIO_SID;
+  // const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require("twilio")(accountSid, authToken);
 
-  return function (options) {
+  // return function (options) {
     client.messages
       .create({
-        body: options.text,
+        body: 'step 1',
         from: "+19388008322",
-        to: options.phoneNumber,
+        to: "+16048805245",
       })
       .then((message) => console.log(message.sid))
-      .catch((error) => console.log(error));
-  };
-};
-const notifySMS = twilioSMSAPI();
+      // .catch((error) => console.log(error));
+  // };
+// };
+// const notifySMS = twilioSMSAPI();
 
-const notifyOwner = (id = 1234, phoneNumber = "+16048805245") => {
-  const options = {
-    text: `New Order is placed`,
-    phoneNumber: phoneNumber,
-  };
-  // notifySMS(options);
-};
+// const notifyOwner = (id = 1234, phoneNumber = "step 2") => {
+//   const options = {
+//     text: `New Order is placed`,
+//     phoneNumber: "+16048805245",
+//   };
+//   // notifySMS(options);
+// };
 
-const notifyCustomerOrderConfirmed = (
-  id = 7,
-  waitTime = 15,
-  phoneNumber = "+16048805245"
-) => {
-  const options = {
-    text: `Your order #${id} is confirmed and will be ready for a pickup in ${waitTime} minutes. See you soon!`,
-    phoneNumber: phoneNumber,
-  };
-  // notifySMS(options);
-};
+// const notifyCustomerOrderConfirmed = (
+//   id = 7,
+//   waitTime = 15,
+//   phoneNumber = "+16048805245"
+// ) => {
+//   const options = {
+//     text: `step 3!`,
+//     phoneNumber: "+16048805245",
+//   };
+//   // notifySMS(options);
+// };
 
-const notifyCustomerOrderReady = (id = 1234, phoneNumber = "+16048805245") => {
-  const options = {
-    text: `Your order #${id} is ready for a pickup.`,
-    phoneNumber,
-  };
-  // notifySMS(options);
-};
+// const notifyCustomerOrderReady = (id = 1234, phoneNumber = "+16048805245") => {
+//   const options = {
+//     text: `Your order is ready for a pickup.`,
+//     phoneNumber,
+//   };
+//   // notifySMS(options);
+// };
 
-module.exports = {
-  notifyOwner,
-  notifyCustomerOrderConfirmed,
-  notifyCustomerOrderReady,
-};
+// module.exports = {
+//   notifyOwner,
+//   notifyCustomerOrderConfirmed,
+//   notifyCustomerOrderReady,
+// };
